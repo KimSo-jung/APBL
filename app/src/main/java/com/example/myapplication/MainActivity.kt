14daa021ea3f7effdccd92ec4811ee87d6a2fcad
KimSo-jung <mobssy21@gmail.com>
2022-11-19 23:50:59 +0900

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) { //added text as a test
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation.selectedItemId = R.id.action_home
     }
 }
